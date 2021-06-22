9cb3728ea34c5a49201433b1d0a707a34b8b965a
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-06-22 21:46:55 +0100

    Start adding build system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,46 @@
 const del = require('del');
-const { dest, series, src, watch } = require('gulp');
+const { dest, series, src, parallel, watch } = require('gulp');
 const ts = require('gulp-typescript')
+const browserSync = require('browser-sync').create();
 
 function clean() {
     return del("built/**");
 }
 
-function build() {
+function buildTs() {
     return src("src/**/*.ts")
         .pipe(ts({
             noImplicitAny: true,
-            outFile: 'code.js'
+            outFile: "code.js"
         }))
-        .pipe(dest('built/static'));
+        .pipe(dest("built/static"));
 }
 
+function buildHTML() {
+    return src("static/**/*.html")
+           .pipe(dest("built/"))
+}
+
+const build = parallel(buildTs, buildHTML);
+
 function watchfiles() {
-    watch("src/**/*.ts", build);
+    watch("src/**/*.ts", series(build, browserSyncReload));
+}
+
+function browserSyncTask(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./built"
+        }
+    });
+    cb();
+}
+
+function browserSyncReload(cb) {
+    browserSync.reload();
+    cb();
 }
 
 exports.build = build;
 exports.watch = watchfiles;
-exports.default = series(clean, build);
+exports.default = series(clean, build, parallel(watchfiles, browserSyncTask));
